61ddb0678e640a0eca6c2149d6d3608dbf96ca0f
abielbagas <abiel.bagas12@gmail.com>
2026-04-11 22:04:32 +0700

    [ADDED] New Commit

---
+++ src/main/java/com/nexstudio/expensetracker_be/ExpensetrackerBeApplication.java
@@ -0,0 +1,13 @@
+package com.nexstudio.expensetracker_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensetrackerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensetrackerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexstudio/expensetracker_be/ExpensetrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexstudio.expensetracker_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensetrackerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
